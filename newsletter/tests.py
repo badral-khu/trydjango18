6c77cc876fa393220a8d3fb9bd73d7312dbbcfd6
badralkh <badralkh@gmail.com>
2016-05-31 17:54:47 +0900

    added a selenuim test

--- newsletter/tests.py
+++ newsletter/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+#!/usr/bin/python
+# coding: utf-8
+# -*- coding: utf-8 -*-
 
-# Create your tests here.
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase, LiveServerTestCase 
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from django.core import mail
+import re
+
+class RegisterTest(LiveServerTestCase): 
+    def setUp(self):
+    	self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(3)
+        self.SERVER_URL=self.live_server_url
+        self.driver.maximize_window()
+        self.driver.get(self.SERVER_URL)
+        mail.outbox=[]
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_register(self):
+		register_in_link = self.driver.find_element_by_link_text("Register")  
+		register_in_link.click()
+		input_username =  self.driver.find_element_by_id("id_username")
+		# print "input_username=", input_username
+		# self.assertEquals(input_username,)
+		self.assertTrue(input_username!=None)
+		
+        # ok_button.click()
+		
+
+
+
+
